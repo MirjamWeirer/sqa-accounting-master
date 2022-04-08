43bd27ef4c33f9a4b35cfbe347a2c8e07cb7eeb9
MirjamWeirer <mirjam.weirer@gmx.at>
2022-04-08 10:01:16 +0200

    add test cases for InputHelper::getDate(); dependency injection of Scanner and System.out

--- src/test/java/at/campus02/input/TestInputHelper.java
+++ src/test/java/at/campus02/input/TestInputHelper.java
@@ -2,6 +2,8 @@ package at.campus02.input;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 
 import java.io.ByteArrayOutputStream;
 import java.io.EOFException;
@@ -10,18 +12,27 @@ import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 public class TestInputHelper {
     @Test
     public void testValidDate() throws EOFException {
         //setup
         Scanner scanner = new Scanner("2022-03-31\n");
-        InputHelper inputHelper = new InputHelper(scanner, System.out);
+        PrintStream mockOut = Mockito.mock(PrintStream.class);
+        InputHelper inputHelper = new InputHelper(scanner, mockOut);
         //execute
         Date result = inputHelper.getDate("test");
         //verify
         Date expected = new Date(122, Calendar.MARCH,31);
-        Assert.assertEquals(expected,result);
+        assertEquals(expected,result);
+        verify(mockOut,Mockito.never()).print(anyString());
+        verify(mockOut,Mockito.atLeastOnce()).println(anyString());
     }
 
     @Test
@@ -35,8 +46,24 @@ public class TestInputHelper {
         Date result = inputHelper.getDate("test");
         //verify
         Date expected = new Date(122, Calendar.MARCH,31);
-        Assert.assertEquals(expected,result);
+        assertEquals(expected,result);
         String msg = outputstream.toString(StandardCharsets.UTF_8);
-        Assert.assertTrue(msg.contains("Wrong date format"));
+        assertTrue(msg.contains("Wrong date format"));
+    }
+
+    @Test
+    public void testValidDateAfterInvalidDate2() throws EOFException {
+        //setup
+        Scanner scanner = mock(Scanner.class);
+        when(scanner.next(anyString())).thenReturn("2022-03-01");
+        PrintStream out = mock(PrintStream.class);
+        InputHelper inputHelper = new InputHelper(scanner,out);
+        //execute
+        Date result = inputHelper.getDate("test");
+        //verify
+        Date expected = new Date(122, Calendar.MARCH,31);
+        assertEquals(expected,result);
+        verify(out, times(1)).println("Wrong date format.");
+        verify(out, times(2)).println("test (empty string to abort) ");
     }
 }
